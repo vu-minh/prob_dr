9ab857272033e84d04e727323641d725daba88af
vu-minh <vuvietminh@unamur.be>
2018-09-05 08:45:28 +0200

    Get cluster assigment from GMM and compare it with sklearn

--- gmm_tfp_simple.py
+++ gmm_tfp_simple.py
@@ -7,7 +7,12 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 from tensorflow_probability import edward2 as ed
+import matplotlib as mpl
 from matplotlib import pyplot as plt
+
+from sklearn.metrics import homogeneity_completeness_v_measure
+from sklearn.mixture import GaussianMixture
+from sklearn.datasets import load_iris, load_digits
 
 tfd = tfp.distributions
 tfb = tfp.bijectors
@@ -18,25 +23,31 @@ print('Tensorflow version: ', tf.__version__)
 tf.reset_default_graph()
 
 dtype = np.float32
-N = 500
-D = 2
-K = 3
+# N = 500
+# D = 2
+# K = 3
 
-# Generate the data
-true_loc = np.array([[-5, 3],
-                     [0, 0],
-                     [2, 2]], dtype)
-random = np.random.RandomState(seed=42)
+# # Generate the data
+# true_loc = np.array([[-5, 3],
+#                      [0, 0],
+#                      [2, 2]], dtype)
+# random = np.random.RandomState(seed=42)
 
-true_hidden_component = random.randint(0, K, N)
-count = [np.count_nonzero(true_hidden_component == k) * 1.0 / N
-         for k in range(K)]
-print(count)
-observations = (true_loc[true_hidden_component] +
-                random.randn(N, D).astype(dtype))
+# true_hidden_component = random.randint(0, K, N)
+# count = [np.count_nonzero(true_hidden_component == k) * 1.0 / N
+#          for k in range(K)]
+# print(count)
+# observations = (true_loc[true_hidden_component] +
+#                 random.randn(N, D).astype(dtype))
+
+observations, true_hidden_component = load_digits(return_X_y=True)
+observations = observations.astype(dtype)
+N, D = observations.shape
+K = 10
 
 # plt.scatter(observations[:, 0], observations[:, 1], c=true_hidden_component)
 # plt.savefig('./plots/gmm_tfp_data.png')
+# plt.gcf().clear()
 
 # PRIOR
 rv_pi = tfd.Dirichlet(
@@ -149,6 +160,7 @@ def inference_map(num_epochs=2000, learning_rate=0.05):
 
     plt.plot(range(len(losses)), losses)
     plt.savefig('./plots/gmm_tfp_simple_loss_map.png')
+    plt.gcf().clear()
 
 
 def inference_vi2(num_epochs=2000, learning_rate=0.05):
@@ -159,10 +171,12 @@ def inference_vi2(num_epochs=2000, learning_rate=0.05):
     qsigma_alpha = tf.nn.softplus(tf.Variable(np.ones([K, D]), dtype=dtype))
     qsigma_beta = tf.nn.softplus(tf.Variable(np.ones([K, D]), dtype=dtype))
 
-    def variational_model(qpi_alpha, qmu_loc, qmu_scale, qsigma_alpha, qsigma_beta):
+    def variational_model(qpi_alpha, qmu_loc, qmu_scale,
+                          qsigma_alpha, qsigma_beta):
         qpi = ed.Dirichlet(concentration=qpi_alpha, name='qpi')
         # This model works well
-        # qmu = ed.MultivariateNormalDiag(loc=qmu_loc, scale_diag=qmu_scale, name='qmu')
+        # qmu = ed.MultivariateNormalDiag(
+        #   loc=qmu_loc, scale_diag=qmu_scale, name='qmu')
         qmu = ed.Normal(loc=qmu_loc, scale=qmu_scale, name='qmu')
         qsigma = ed.InverseGamma(concentration=qsigma_alpha, rate=qsigma_beta,
                                  name='qsigma')
@@ -197,12 +211,13 @@ def inference_vi2(num_epochs=2000, learning_rate=0.05):
             qpi, qmu, qsigma
         ])
 
-    print(posterior_mu)
-    # plt.plot(range(len(losses)), losses)
-    # plt.savefig('./plots/gmm_tfp_simples_loss_vi2.png')
+    # print(posterior_mu)
+    plt.plot(range(len(losses)), losses)
+    plt.savefig('./plots/gmm_tfp_simples_loss_vi2.png')
+    plt.gcf().clear()
 
     # Model criticism
-    generate_process= tfd.MixtureSameFamily(
+    generate_process = tfd.MixtureSameFamily(
         mixture_distribution=tfd.Categorical(probs=posterior_pi),
         components_distribution=tfd.MultivariateNormalDiag(
             loc=posterior_mu,
@@ -210,18 +225,28 @@ def inference_vi2(num_epochs=2000, learning_rate=0.05):
         )
     )
 
-    n_samples = 500
     with tf.Session() as sess:
-        x_generated = generate_process.sample(n_samples).eval()
-    return x_generated, posterior_pi, posterior_mu, posterior_sigma
+        # get cluster for observed data
+        observations_pad = generate_process._pad_sample_dims(
+            tf.convert_to_tensor(observations))
+        log_prob_x = generate_process.components_distribution._log_prob(
+            observations_pad)
+        clusters = tf.argmax(log_prob_x, axis=1).eval()
 
+        # test generate some new data
+        n_samples = 250
+        x_generated = generate_process.sample(n_samples)
+        x_generated = x_generated.eval()
+    return x_generated, clusters, posterior_pi, posterior_mu, posterior_sigma
 
 
 def inference_vi1(num_epochs=2000, learning_rate=0.05):
     def variational_model():
-        qpi = tf.nn.softplus(tf.Variable(np.zeros(K) * 1.0 / K, dtype=dtype), name='qpi')
+        qpi = tf.nn.softplus(tf.Variable(
+            np.zeros(K) * 1.0 / K, dtype=dtype), name='qpi')
         qmu = tf.Variable(np.zeros([K, D]), dtype=dtype, name='qmu')
-        qsigma = tf.nn.softplus(tf.Variable(np.ones([K, D]), dtype=dtype, name='qsigma'))
+        qsigma = tf.nn.softplus(tf.Variable(
+            np.ones([K, D]), dtype=dtype, name='qsigma'))
         return qpi, qmu, qsigma
 
     log_q = ed.make_log_joint_fn(variational_model)
@@ -253,40 +278,55 @@ def inference_vi1(num_epochs=2000, learning_rate=0.05):
     plt.savefig('./plots/gmm_tfp_simples_loss_vi1.png')
 
 
-import matplotlib as mpl
-
 def make_ellipses(means, covs, ax):
     # http://scikit-learn.org/stable/auto_examples/mixture/plot_gmm_covariances.html
     colors = ['navy', 'turquoise', 'darkorange']
     for n, color in enumerate(colors):
-
         covariances = np.diag(covs[n][:2])
-
         v, w = np.linalg.eigh(covariances)
         u = w[0] / np.linalg.norm(w[0])
         angle = np.arctan2(u[1], u[0])
         angle = 180 * angle / np.pi  # convert to degrees
         v = 2. * np.sqrt(2.) * np.sqrt(v)
         ell = mpl.patches.Ellipse(means[n, :2], v[0], v[1],
-                                    180 + angle, color=color)
+                                  180 + angle, color=color)
         ell.set_clip_box(ax.bbox)
         ell.set_alpha(0.5)
         ax.add_artist(ell)
 
+
 def main():
     # inference_map(num_epochs=2500, learning_rate=0.01)
-    x_generated, pi, mu, sigma = inference_vi2(num_epochs=2000, learning_rate=0.075)
-
+    x_generated, predicted_clusters, pi, mu, sigma = inference_vi2(
+        num_epochs=5000, learning_rate=0.01)
     print('Cluster proportion: ', pi)
-    
-    _, ax = plt.subplots()
-    plt.scatter(observations[:, 0], observations[:, 1], c=true_hidden_component, alpha=0.2)
-    plt.scatter(x_generated[:, 0], x_generated[:, 1], marker='x')
-    plt.scatter(mu[:, 0], mu[:, 1], marker='*', c='black')
-    make_ellipses(means=mu, covs=sigma, ax=ax)
 
-    plt.savefig('./plots/gmm_tfp_simple_generate1.png')
+    hcv = homogeneity_completeness_v_measure(
+        labels_true=true_hidden_component, labels_pred=predicted_clusters)
+    print('MyGMM homogeneity={}, completeness={}, v_measure={}'.format(*hcv))
 
+    # _, ax = plt.subplots()
+    # plt.scatter(observations[:, 0], observations[:, 1],
+    #             c=true_hidden_component, alpha=0.4)
+    # plt.scatter(x_generated[:, 0], x_generated[:, 1], marker='x')
+    # plt.scatter(mu[:, 0], mu[:, 1], marker='*', c='black', alpha=0.2)
+    # make_ellipses(means=mu, covs=sigma, ax=ax)
+
+    # plt.savefig('./plots/gmm_tfp_simple_generate1.png')
+    # plt.gcf().clear()
+
+
+def gmm_sklearn():
+    estimator = GaussianMixture(n_components=K, covariance_type='diag',
+                                max_iter=50, random_state=0)
+    estimator.fit(observations)
+    predicted_clusters = estimator.predict(observations)
+
+    hcv = homogeneity_completeness_v_measure(
+        labels_true=true_hidden_component, labels_pred=predicted_clusters)
+    print('SklearnGMM: homogeneity={}, completeness={}, v_measure={}'.format(*hcv))
 
 if __name__ == '__main__':
     main()
+    # gmm_sklearn()
+    # SklearnGMM: homogeneity=0.603092532433, completeness=0.6219483973602182, v_measure=0.6123753498912625
