824f8129a45f05b4365d790dcb012d1fd80fdfa1
vu-minh <vuvietminh@unamur.be>
2018-09-11 10:41:56 +0200

    PCA then GMM in sklearn

---
+++ sklearn_test.py
@@ -0,0 +1,72 @@
+"""PCA and GMM using sklearn
+"""
+
+
+import numpy as np
+
+from sklearn.datasets import load_iris, load_digits, load_breast_cancer, load_wine
+from sklearn.metrics import homogeneity_completeness_v_measure as hcv_measure
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.mixture import GaussianMixture
+
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+
+def load_dataset(id=0):
+    load_funcs = [
+        load_iris,
+        load_digits,
+        load_wine,
+        load_breast_cancer,
+    ]
+    dataset = load_funcs[id](return_X_y=False)
+    X = dataset['data']
+    y = dataset['target']
+    K = len(dataset['target_names'])
+    return X, y, K
+
+
+def measure_cluster(labels_true, labels_pred, msg='Clustering measures'):
+    print('\n' + msg + ':')
+    hcv = hcv_measure(labels_true, labels_pred)
+    print('Clustering measures: '
+          'hom={:.3f}, com={:.3f}, v-measure={:.3f}'.format(*hcv))
+
+
+def evaluate(dataset_name, id):
+    X, y, K = load_dataset(id)
+
+    gmm = GaussianMixture(n_components=K, covariance_type='diag')
+    gmm.fit(X)
+    predicted_labels = gmm.predict(X)
+    measure_cluster(y, predicted_labels, msg='GMM on the original dataset')
+
+    pca = PCA(n_components=K)
+    X2d = pca.fit_transform(X)
+    gmm2 = GaussianMixture(n_components=K, covariance_type='diag')
+    gmm2.fit(X2d)
+    predicted_labels2 = gmm2.predict(X2d)
+    measure_cluster(y, predicted_labels2, msg='PCA then GMM on reduced data')
+    plt.scatter(X2d[:, 0], X2d[:, 1], marker='o', color='white', alpha=1.0,
+                linewidths=1, s=64,
+                cmap='tab10', edgecolors=cm.tab10(y))
+    plt.scatter(X2d[:, 0], X2d[:, 1], c=predicted_labels2, s=16, alpha=0.8,
+                cmap='tab20b')
+    plt.savefig('./plots/pca_gmm_{}.png'.format(dataset_name))
+
+
+def main():
+    # datasets = [
+    #     'IRIS', 'DIGITS', 'WINE', 'BREAST_CANCER'
+    # ]
+    # for dataset_id, dataset_name in enumerate(datasets):
+    #     print('Dataset: {}'.format(dataset_name))
+    #     evaluate(dataset_name, dataset_id)
+
+    evaluate('WINE', 2)
+
+
+if __name__ == '__main__':
+    main()
